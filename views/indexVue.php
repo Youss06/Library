caaaf0e6bde33e7f6b85c8cde3f5ba5bea26e253
Youss06 <oumar.youssouf0605@gmail.com>
2017-11-27 20:06:42 +0100

    Ad Html parallax

---
+++ views/indexVue.php
@@ -0,0 +1,43 @@
+<?php
+include("template/header.php");
+
+?>
+
+<div class="img1">
+   <div class="text">
+       <span class="border">
+     Library
+       </span>
+   </div>
+</div>
+
+<section class="section Firstsection">
+  <h2>Bienvenue</h2>
+  <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> -->
+</section>
+
+<div class="img2">
+   <div class="text">
+       <span class="border trans">
+
+       </span>
+   </div>
+</div>
+
+<section class="section Secondsection">
+  <h2>Bienvenue 2</h2>
+  <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> -->
+</section>
+
+<div class="img3">
+   <div class="text">
+       <span class="border trans">
+
+       </span>
+   </div>
+</div>
+
+<section class="section Thirdsection">
+  <h2>Bienvenue 3</h2>
+  <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> -->
+</section>
