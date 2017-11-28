6e6ad8fe32788000848c18f2dfd679c6286b2375
Youss06 <oumar.youssouf0605@gmail.com>
2017-11-28 16:46:47 +0100

    Add html page dorm and update

--- views/indexVue.php
+++ views/indexVue.php
@@ -12,7 +12,7 @@ include("template/header.php");
 </div>
 
 <section class="section Firstsection">
-  <h2>Bienvenue</h2>
+  <h2>Nos Livres</h2>
   <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> -->
 </section>
 
@@ -25,7 +25,32 @@ include("template/header.php");
 </div>
 
 <section class="section Secondsection">
-  <h2>Bienvenue 2</h2>
+  <h2>Form</h2>
+  <div class="form-group">
+  <label for="exampleInputEmail1">Votre Nom</label>
+  <input type="text" class="form-control" name="name" id="formGroupExampleInput" placeholder="Votre nom">
+  <small id="emailHelp" class="form-text text-muted">Veuillez indiquer votre nom</small>
+</div>
+
+
+
+<form class="" action="" method="post" enctype="multipart/form-data">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Votre Prénom</label>
+    <input type="text" class="form-control" id="formGroupExampleInput" name="firstName" placeholder="Votre prénom">
+    <small id="emailHelp" class="form-text text-muted">Veuillez indiqer votre prénom.</small>
+  </div>
+
+  <form class="" action="" method="post" enctype="multipart/form-data">
+    <div class="form-group">
+      <label for="exampleInputEmail1">Sommes</label>
+      <input type="text" class="form-control" id="formGroupExampleInput" name="amount" placeholder="Ex : 1000">
+      <small id="emailHelp" class="form-text text-muted">Veuillez insérer le montant que vous souhaiter ajouter à votre compte</small>
+    </div>
+
+
+
+    <button type="submit" name="send" class="btn btn-primary">Envoyer</button>
   <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> -->
 </section>
 
@@ -38,6 +63,70 @@ include("template/header.php");
 </div>
 
 <section class="section Thirdsection">
-  <h2>Bienvenue 3</h2>
+  <h2>Description</h2>
+  <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> -->
+</section>
+
+<div class="img4">
+   <div class="text">
+       <span class="border trans">
+
+       </span>
+   </div>
+</div>
+
+<section class="section Fourthsection">
+  <h2>Update</h2>
+  <div class="form-group">
+  <label for="exampleInputEmail1">Votre Nom</label>
+  <input type="text" class="form-control" name="name" id="formGroupExampleInput" placeholder="Votre nom">
+  <small id="emailHelp" class="form-text text-muted">Veuillez indiquer votre nom</small>
+</div>
+
+
+
+<form class="" action="" method="post" enctype="multipart/form-data">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Votre Prénom</label>
+    <input type="text" class="form-control" id="formGroupExampleInput" name="firstName" placeholder="Votre prénom">
+    <small id="emailHelp" class="form-text text-muted">Veuillez indiqer votre prénom.</small>
+  </div>
+
+  <form class="" action="" method="post" enctype="multipart/form-data">
+    <div class="form-group">
+      <label for="exampleInputEmail1">Sommes</label>
+      <input type="text" class="form-control" id="formGroupExampleInput" name="amount" placeholder="Ex : 1000">
+      <small id="emailHelp" class="form-text text-muted">Veuillez insérer le montant que vous souhaiter ajouter à votre compte</small>
+    </div>
+
+
+
+    <button type="submit" name="send" class="btn btn-primary">Envoyer</button>
+  <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> -->
+</section>
+
+<div class="img5">
+   <div class="text">
+       <span class="border trans">
+
+       </span>
+   </div>
+</div>
+
+<section class="section Fifthsection">
+  <h2>Utilisateurs</h2>
+  <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> -->
+</section>
+
+<div class="img6">
+   <div class="text">
+       <span class="border trans">
+
+       </span>
+   </div>
+</div>
+
+<section class="section Sixthsection">
+  <h2>Tri</h2>
   <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> -->
 </section>
